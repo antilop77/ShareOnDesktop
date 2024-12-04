d878be1d4b6712d8a539ce3c99ab2f0080f7d4f4
idris boz <pacificoceanido@gmail.com>
2024-12-04 18:22:10 +0300

    t

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace ShareOnDeskTop
 {
@@ -16,6 +17,10 @@ namespace ShareOnDeskTop
         {
             Common.dbServer = args[0];
             Common.dbName = args[1];
+            Common.dbPassword = args[2];
+            //;Password=zeka7744;Trusted_Connection=True;TrustServerCertificate=True;
+            Common._connectionString = "Server=" + Common.dbServer + ";Database=" + Common.dbName + ";User Id=sa" + Common.dbPassword;
+        
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
